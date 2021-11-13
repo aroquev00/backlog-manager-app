714acdbe266ddc3f709839da96aa71a386408649
eduardo139 <A00826142@itesm.mx>
2021-11-13 16:07:07 -0600

    Diseño de página de Cliente

--- src/modules/clientes/buscar/modules/BuscarCliente.jsx
+++ src/modules/clientes/buscar/modules/BuscarCliente.jsx
@@ -1,10 +1,123 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Stack, Grid, Card, CardContent, CardActions, TextField, Button } from "@mui/material";
 import React from "react";
 
 export default function BuscarCliente() {
   return(
     <Container maxWidth="lg" style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-      <Typography variant="h3" style={{margin: '10px'}}> Buscar cliente </Typography>
+      <Stack spacing={3} mb={10}>
+        <Typography variant="h3" style={{margin: '30px 10px 10px'}}> Buscar cliente </Typography>
+
+        <TextField label="Nombre del cliente" margin="normal" />
+        <Button variant="contained" size="large"
+          onClick={() => {
+            alert('clicked');
+          }}
+        > 
+          Buscar
+        </Button>
+      </Stack>
+
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nombre del cliente
+              </Typography>
+              <br />
+              <Typography variant="body2">
+                Teléfono del cliente
+              </Typography>
+              <Typography variant="body2">
+                E-mail del cliente
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Editar datos</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nombre del cliente
+              </Typography>
+              <br />
+              <Typography variant="body2">
+                Teléfono del cliente
+              </Typography>
+              <Typography variant="body2">
+                E-mail del cliente
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Editar datos</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nombre del cliente
+              </Typography>
+              <br />
+              <Typography variant="body2">
+                Teléfono del cliente
+              </Typography>
+              <Typography variant="body2">
+                E-mail del cliente
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Editar datos</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nombre del cliente
+              </Typography>
+              <br />
+              <Typography variant="body2">
+                Teléfono del cliente
+              </Typography>
+              <Typography variant="body2">
+                E-mail del cliente
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Editar datos</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={3}>
+          <Card variant="outlined" sx={{ minWidth: 275 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Nombre del cliente
+              </Typography>
+              <br />
+              <Typography variant="body2">
+                Teléfono del cliente
+              </Typography>
+              <Typography variant="body2">
+                E-mail del cliente
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Editar datos</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
--- src/modules/clientes/registrar/modules/RegistrarCliente.jsx
+++ src/modules/clientes/registrar/modules/RegistrarCliente.jsx
@@ -1,10 +1,35 @@
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Stack, TextField, Button } from "@mui/material";
 import React from "react";
+
+/*
+import fire from "fire";
+
+Quizá así se agregan datos en Firebase?
+function crearCliente(cid, nombre, telefono, email) {
+  firebase.database().ref('clientes/' + cid).push({
+    nombre: nombre,
+    telefono: telefono,
+    email: email
+  });
+}
+*/
 
 export default function RegistrarCliente() {
   return(
-    <Container maxWidth="lg" style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-      <Typography variant="h3" style={{margin: '10px'}}> Registrar cliente </Typography>
-    </Container>
+      <Container maxWidth="lg" style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+        <Stack spacing={3}>
+          <Typography variant="h3" style={{margin: '30px 10px 10px'}}> Registrar cliente </Typography>
+          <TextField label="Nombre del cliente" />
+          <TextField label="Teléfono del cliente" />
+          <TextField label="Email del cliente" />
+          <Button variant="contained" style={{marginTop: '50px'}} size="large"
+            onClick={() => {
+              alert('clicked');
+            }}
+          > 
+            Registrar
+          </Button>
+        </Stack>
+      </Container>
   );
 }
